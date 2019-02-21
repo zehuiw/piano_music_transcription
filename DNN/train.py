eb1bd5688a944283ac517a6704c0e39f37a6b98d
Zehui <wzehui@stanford.edu>
2019-02-21 15:55:28 -0800

    change loss

--- DNN/train.py
+++ DNN/train.py
@@ -29,50 +29,59 @@ contador_bad = 0
 data_directory = sys.argv[1]
 weights_dir = sys.argv[2]
 
-print 'Build model...' 
-model = Sequential()
-history = History()
 
-print 'Load validation data...'
-X_val = np.load(data_directory + "dev/" + str(0) + "dev_X.npy" )
-y_val = np.load(data_directory + "dev/" + str(0) + "dev_y.npy" )
+def load_data(data_directory):
+    print 'Load validation data...'
+    X_val = np.load(data_directory + "dev/" + str(0) + "dev_X.npy" )
+    y_val = np.load(data_directory + "dev/" + str(0) + "dev_y.npy" )
 
-# Count the number of files in the training folder 
-num_tr_batches = len([name for name in os.listdir(data_directory + "train/")])/2
+    # Count the number of files in the training folder 
+    num_tr_batches = len([name for name in os.listdir(data_directory + "train/")])/2
 
-print 'Loading all data'
-for i in range(num_tr_batches):
-    print "Batching..." + str(i) + "train_X.npy"
-    X_train = np.array(np.load(data_directory + "train/" + str(i) + "train_X.npy" ))
-    y_train = np.array(np.load(data_directory + "train/" + str(i) + "train_y.npy" ))
-    if i == 0:
-        X = X_train
-        y = y_train
-    else:
-        X = np.concatenate((X,X_train), axis = 0)
-        y = np.concatenate((y,y_train), axis = 0)
+    print 'Loading training data'
+    for i in range(num_tr_batches):
+        print "Batching..." + str(i) + "train_X.npy"
+        X_train = np.array(np.load(data_directory + "train/" + str(i) + "train_X.npy" ))
+        y_train = np.array(np.load(data_directory + "train/" + str(i) + "train_y.npy" ))
+        if i == 0:
+            X = X_train
+            y = y_train
+        else:
+            X = np.concatenate((X,X_train), axis = 0)
+            y = np.concatenate((y,y_train), axis = 0)
+    print X.shape
+    return X_val, y_val, X, y
 
-print X.shape
-print "Adding 1st layer of {} units".format(number_units)
-model.add(Dense(number_units, input_shape=(input_size,), kernel_initializer='normal', activation='relu'))
-model.add(Dropout(0.2))
-for i in range(number_layers-1):
-    print "Adding %d" % (i+2) + "th layer of %d" % number_units + " units"
-    model.add(Dense(number_units, kernel_initializer='normal', activation='relu'))
+def build_model(model):
+    print "Adding 1st layer of {} units".format(number_units)
+    model.add(Dense(number_units, input_shape=(input_size,), kernel_initializer='normal', activation='relu'))
     model.add(Dropout(0.2))
+    for i in range(number_layers-1):
+        print "Adding %d" % (i+2) + "th layer of %d" % number_units + " units"
+        model.add(Dense(number_units, kernel_initializer='normal', activation='relu'))
+        model.add(Dropout(0.2))
 
-print " Adding classification layer"
-model.add(Dense(number_classes, kernel_initializer='normal', activation='sigmoid'))
+    print " Adding classification layer"
+    model.add(Dense(number_classes, kernel_initializer='normal', activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Compile model
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error'])
 
-checkpointer = ModelCheckpoint(filepath= weights_dir + "weights.hdf5", verbose=1, save_best_only=False)
-early = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, mode='auto')
+def train(data_directory, weights_dir):
+    X_val, y_val, X, y = load_data(data_directory)
+    model = Sequential()
+    history = History()
+    build_model(model)
 
-training_log = open(weights_dir + "Training.log", "w")
-print 'Train . . .'
+    checkpointer = ModelCheckpoint(filepath= weights_dir + "weights.hdf5", verbose=1, save_best_only=False)
+    early = EarlyStopping(monitor='val_loss', min_delta=0, patience=40, verbose=1, mode='auto')
+
+    training_log = open(weights_dir + "Training.log", "w")
+    print 'Train . . .'
 # let's say you have an ImageNet generat        print "Fitting the batch :"
-save = model.fit(X, y,batch_size=mini_batch_size,epochs = num_epochs,validation_data=(X_val, y_val),verbose=1,callbacks=[checkpointer,early])
-training_log.write(str(save.history) + "\n")
-training_log.close()
+    save = model.fit(X, y,batch_size=mini_batch_size,epochs = num_epochs,validation_data=(X_val, y_val),verbose=1,callbacks=[checkpointer,early])
+    training_log.write(str(save.history) + "\n")
+    training_log.close()
+
+if __name__=="__main__":
+    train(data_directory, weights_dir)
